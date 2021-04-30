d5576878d41831a3fb719abe16587bddea3d0cc9
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-04-29 21:47:48 -0300

    Hero

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import Navbar from './components/Navbar';
 import "./App.scss";
+import Hero from './components/Hero';
 
 const App = () => (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
 )
 
